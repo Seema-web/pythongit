854a97a874998624df4d64d11566013e679fa315
seema-web <seemamehta1123@gmail.com>
2021-03-06 22:21:35 +0530

    gitt

--- hungrii.py
+++ hungrii.py
@@ -2,6 +2,7 @@ hungry=input("R u hungry")
 if hungry=='yes':
     print("eat pizza")
     print('eat momos')
+    print('eat rajma' )
 else:
     thristy=input('R u thristy')
     if thristy=='yes':
